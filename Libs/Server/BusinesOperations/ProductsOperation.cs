4efa7847a291bf297f5f0fdad5d313245c1f4646
Jose <josepe1000es@gmail.com>
2014-08-11 16:19:39 +0200

    Add interface for ProductsOperations

--- Libs/Server/BusinesOperations/ProductsOperation.cs
+++ Libs/Server/BusinesOperations/ProductsOperation.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AdministrationCenter;
+using BusinesOperations.Interfaces;
 
 namespace Camiher.Libs.Server.BusinesOperations
 {
-    public class ProductsOperation
+    public class ProductsOperation:IProductsOperations
     {
+        /// <summary>
+        /// Get a product by its productID
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
         public ProductsSet GetProductById(string productId)
         {
-            var _dataDC = new Model1Container();
-            ProductsSet Product = _dataDC.ProductsSet.First(S => S.Id == productId);
-            return Product;
+            var dataDc = new Model1Container();
+            ProductsSet product = dataDc.ProductsSet.First(s => s.Id == productId);
+            return product;
         }
     }
 }
